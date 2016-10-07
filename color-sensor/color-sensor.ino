8dd587e1d6b309b8839687fc69d4148836b09dfa
rustechs <rus.tech.studio@gmail.com>
2016-10-07 01:29:33 -0400

    Added color-sensor.ino

---
+++ color-sensor/color-sensor.ino
@@ -0,0 +1,23 @@
+/*
+ * color-sensor.ino
+ * Samples four amplified photodiodes (IR, R, Y, G) using
+ * ADCs, averages samples, and transmits data over USB. Also
+ * controls light source -- white LED or RGB.
+ * 
+ * Alexander Volkov Jr.
+ * 
+ * 16-722
+ * Assignment 3: Color Sensing
+ */
+
+void setup() {
+
+  
+
+}
+
+void loop() {
+
+  
+
+}
